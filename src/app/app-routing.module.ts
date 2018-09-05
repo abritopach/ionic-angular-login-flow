59da8db6265e09314f790f2f93da4c2ebf17b1d8
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-05 16:09:09 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
+  { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' },
 ];
 
 @NgModule({
